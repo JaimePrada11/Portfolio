361e526767d850559ea1130b327a12b09c219d93
JaimePrada11 <jandrespradato.11@gmail.com>
2025-01-14 23:33:48 -0500

    feat: :sparkles: Skills component
    bby @JaimePrada11

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Certificates from './components/Certificates/Certificates'
 import GridSkills from './components/Projects/ProjectCard'
 import Projects from './components/Projects/Projects'
+import Skills from './components/Skill'
 
 function App() {
 
   return (
     <>
     <h1 className='text-yellow-500'>basicamente es !!Hola</h1>
+    <Skills/>
     <Projects/>
     <Certificates/>
     </>
